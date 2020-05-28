8bf2a354ee0a2b4622b8b3061b770e33d4e0d201
MarcosRuginski <marcosrobertoruginski@gmail.com>
2020-05-28 20:38:46 -0300

    project settings

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Wo1rld' });
-})
+app.get('/', (request, response) => response.json({ message: 'Hello Wo1rld' }));
 
 app.listen(3333, () => {
   console.log('🚚 Server started on port 3333!');
