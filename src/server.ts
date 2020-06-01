82799870690e28efeba5503220752e8bfda1a67e
MarcosRuginski <marcosrobertoruginski@gmail.com>
2020-06-01 20:45:17 -0300

    settings of eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello Wo1rld' }));
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚚 Server started on port 3333!');
